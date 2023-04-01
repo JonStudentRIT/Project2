379d48b1cf24fa7bc32deeed4275fe53b4a7bfe7
JonStudentRIT <jk9927@g.rit.edu>
2023-03-31 21:44:45 -0400

    domo b

--- server/app.js
+++ server/app.js
@@ -6,14 +6,15 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb+srv://jk9927:S4mVxyzHm9LqpJdU@cluster0.mcyfvky.mongodb.net/DomoA?retryWrites=true&w=majority';//'mongodb://127.0.0.1/DomoMaker';
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://jk9927:S4mVxyzHm9LqpJdU@cluster0.mcyfvky.mongodb.net/DomoA?retryWrites=true&w=majority';// 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
-  if(err) {
+  if (err) {
     console.log('Could not connect to database');
     throw err;
   }
@@ -27,6 +28,14 @@ app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(session({
+  key: 'sessionid',
+  secret: 'Domo Arigato', // mr roboto
+  resave: false,
+  saveUninitialized: false,
+}));
+
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,2 +1,2 @@
 module.exports.Account = require('./Account.js');
-module.exports.Domo = require('./Domo.js');
+module.exports.Domo = require('./Domo.js'); // step 14 on domo a and step 10 on domo b repeat
