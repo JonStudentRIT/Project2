65ccf1949976a609bb8e99514ff0b19fa287aa61
JonStudentRIT <jk9927@g.rit.edu>
2023-04-05 16:11:29 -0400

    bug fix

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -2,25 +2,13 @@ const models = require('../models');
 
 const { Domo } = models;
 
-const makerPage = async (req, res) => {
-  return res.render('app');
-  // try {
-  //   const query = { owner: req.session.account._id };
-  //   const docs = await Domo.find(query).select('name age').lean().exec();
-
-  //   res.render('app', { domos: docs });
-  // } catch (err) {
-  //   console.log(err);
-  //   return res.status(500).json({ error: 'Error retrieving domos!' });
-  // }
-  // return res.status(500); // needs a final return
-};
+const makerPage = async (req, res) => res.render('app');
 
 const getDomos = async (req, res) => {
   try {
     const query = { owner: req.session.account._id };
     const docs = await Domo.find(query).select('name age').lean().exec();
-    return res.json({domos: docs});
+    return res.json({ domos: docs });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: 'Error retrieving domos!' });
@@ -41,8 +29,8 @@ const makeDomo = async (req, res) => {
   try {
     const newDomo = new Domo(domoData);
     await newDomo.save();
-    //return res.json({ redirect: '/maker' });
-    return res.status(201).json({name: newDomo.name, age: newDomo.age});
+    // return res.json({ redirect: '/maker' });
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age });
   } catch (err) {
     console.log(err);
     if (err.code === 11000) {
